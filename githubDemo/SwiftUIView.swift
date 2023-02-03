61a93a8a78ac1bbd6a7db7151e40546e225cdbaa
Mayur <mayurtyagi@unolo.com>
2023-02-03 14:27:17 +0530

    2nd view added

---
+++ githubDemo/SwiftUIView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  githubDemo
+//
+//  Created by Mayur  on 03/02/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello second View")
+        
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+        
+    }
+}
